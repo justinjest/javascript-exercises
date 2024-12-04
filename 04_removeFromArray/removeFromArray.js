a50e2a2c00dd433de26dcda9fd112bf583c8a919
justinjest <jest.cayaban@gmail.com>
2024-12-04 10:26:18 +0900

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...toRemove) {
+    let tmp = array
+    let len = toRemove.length
+    for (let i = 0; i < len; i++) {
+        while (tmp.includes(toRemove[i])) {
+            tmp = removeSingleElem(tmp, toRemove[i])
+        }
+    }
+    return tmp
 };
+
+
+function removeSingleElem(array, toRemove) {
+    let tmp = array
+    let index = array.indexOf(toRemove)
+    if (index !== -1) {
+        tmp.splice(index, 1);
+    }
+    return tmp
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
